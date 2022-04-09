98ef3b5c670395ab0cb78015d34e503c5d472704
lucprz <lucasprz.gm@gmail.com>
2022-04-09 16:01:17 -0300

    footer component finished

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import "./footer.scss";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
-  return <footer id="footer">Footer</footer>;
+  return (
+    <footer id="footer">
+      <a href="#" className="footer__logo">
+        LucPrz
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer__socials">
+        <a href="https://linkedin.com" target="_blank">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com" target="_blank">
+          <FaGithub />
+        </a>
+        <a href="https://instagram.com" target="_blank">
+          <FaInstagram />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; LucPrz portfolio</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
